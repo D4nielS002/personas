c342669b92af08693707e1154bf9970df91cc89b
D4nielS002 <ds4472718@gmail.com>
2024-12-11 09:18:09 -0500

    Agregar archivos de proyecto.

---
+++ personas/Controllers/ImcController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using personas.Models;
+using System.Collections.Generic;
+
+namespace personas.Controllers
+{
+    public class ImcController : Controller
+    {
+        public static List <PersonaModel> personas = new List <PersonaModel> ();
+        [HttpPost]
+        public IActionResult Index(PersonaModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                personas.Add(model);
+                return RedirectToAction("Lista");
+            }
+            
+            return View(model);
+        }
+        public IActionResult lista()
+        {
+            return View(personas);
+        }
+    }
+}
